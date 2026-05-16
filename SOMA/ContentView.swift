fe14624c5ced216886b8f5cb1f9aa70637ecedac
umeryo34 <ryo34ume@icloud.com>
2026-05-16 16:42:21 +0900

    Initial Commit

---
+++ SOMA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SOMA
+//
+//  Created by 梅澤遼 on 2026/05/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SOMA/SOMAApp.swift
@@ -0,0 +1,17 @@
+//
+//  SOMAApp.swift
+//  SOMA
+//
+//  Created by 梅澤遼 on 2026/05/16.
+//
+
+import SwiftUI
+
+@main
+struct SOMAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
